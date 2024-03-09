f917a48f542e9f63c41ae918041965a423ee63e6
Devohka <110058157+Devohka@users.noreply.github.com>
2024-03-09 10:34:12 +0200

    odkefdlcv/x.vbnklzc

--- index.js
+++ index.js
@@ -3,44 +3,36 @@ const hoursEl = document.querySelector("[data-hours]");
 const minsEl = document.querySelector("[data-mins]");
 const secsEl = document.querySelector("[data-secs]");
 
-function removeElTimer() {
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minsEl.textContent = mins;
-    secsEl.textContent = secs;
+
+
+class CountdownTimer {
+    constructor({ selector, targetDate }) {
+        this.selector = document.querySelector(selector);
+        this.targetDate = targetDate;
+    };
+    timer() {
+        const inId = setInterval(() => {
+            let time = this.targetDate - Date.now();
+            if (time <= 0) {
+                clearInterval(inId);
+                return;
+            };
+
+            const days = Math.floor(time / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+            const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+            daysEl.textContent = days;
+            hoursEl.textContent = hours;
+            minsEl.textContent = mins;
+            secsEl.textContent = secs;
+        }, 1000);
+    };
 };
+const timerEl = new CountdownTimer({
+    selector: '#timer-1',
+    targetDate: new Date('Jul 17, 2024'),
+});
 
-// new CountdownTimer({
-    // selector: '#timer-1',
-    // targetDate: new Date('Jul 17, 2019'),
-// });
-
-let time =  (new Date('Jul 17, 2019') - new Date().getTime()) * -1;
-
-/*
-* Дні, що залишилися: ділимо значення UTC на 1000 * 60 * 60 * 24, кількість
-* мілісекунд в один день (мілісекунди * секунди * хвилини * години)
-*/
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Решта годин: отримуємо залишок від попереднього розрахунку за допомогою оператора
- * залишку% і ділимо його на кількість мілісекунд в одній годині
- * (1000 * 60 * 60 = мілісекунди * хвилини * секунди)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Решта хвилин: отримуємо хвилини, що залишилися і ділимо їх на кількість
- * мілісекунд в одній хвилині (1000 * 60 = мілісекунди * секунди)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Решта секунд: отримуємо секунди, які залишилися і ділимо їх на кількість
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-
-removeElTimer();
+timerEl.timer()
